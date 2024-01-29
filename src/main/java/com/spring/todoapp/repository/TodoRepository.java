7cc9fc4347fa6bde233552c346c9f2777178b872
Hu6r1s <darmaa333@gmail.com>
2024-01-29 15:50:19 +0900

    fix: query method type

--- src/main/java/com/spring/todoapp/repository/TodoRepository.java
+++ src/main/java/com/spring/todoapp/repository/TodoRepository.java
@@ -1,10 +1,9 @@
 package com.spring.todoapp.repository;
 
-import com.spring.todoapp.dto.TodoResponseDto;
 import com.spring.todoapp.entity.Todo;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-    List<TodoResponseDto> findAllByOrderByCreatedAtDesc();
+    List<Todo> findAllByOrderByCreatedAtDesc();
 }

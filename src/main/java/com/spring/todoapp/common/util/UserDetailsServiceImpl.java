34b604c760b12ec3209463bf612120afb4c11bf9
Hu6r1s <darmaa333@gmail.com>
2024-01-31 17:59:22 +0900

    feat: userDetailsService loadUserByUsername

---
+++ src/main/java/com/spring/todoapp/common/util/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.spring.todoapp.common.util;
+
+import com.spring.todoapp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) {
+                return userRepository.findByUsername(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        };
+    }
+}

1b076cbd87225b460dc1e989c20fb316410a7d78
Hu6r1s <darmaa333@gmail.com>
2024-02-01 14:16:53 +0900

    fix: todo api uri change

--- src/main/java/com/spring/todoapp/controller/TodoController.java
+++ src/main/java/com/spring/todoapp/controller/TodoController.java
@@ -5,6 +5,8 @@ import com.spring.todoapp.dto.TodoResponseDto;
 import com.spring.todoapp.service.TodoService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -15,32 +17,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1/todo")
 @RequiredArgsConstructor
 public class TodoController {
     private final TodoService todoService;
 
-    @PostMapping("/todo")
-    public TodoResponseDto create(@RequestBody TodoRequestDto requestDto) {
-        return todoService.create(requestDto);
+    @PostMapping("/")
+    public TodoResponseDto create(@RequestBody TodoRequestDto requestDto, @AuthenticationPrincipal UserDetails userDetails) {
+        return todoService.create(requestDto, userDetails);
     }
 
-    @GetMapping("/todos")
+    @GetMapping("/")
     public List<TodoResponseDto> findAll() {
         return todoService.findAll();
     }
 
-    @GetMapping("/todo/{id}")
+    @GetMapping("/{id}")
     public TodoResponseDto findById(@PathVariable Long id) {
         return todoService.findById(id);
     }
 
-    @PatchMapping("/todo/{id}")
+    @PatchMapping("/{id}")
     public TodoResponseDto update(@PathVariable Long id, @RequestBody TodoRequestDto requestDto) {
         return todoService.update(id, requestDto);
     }
 
-    @DeleteMapping("/todo/{id}")
+    @DeleteMapping("/{id}")
     public TodoResponseDto delete(@PathVariable Long id, @RequestBody String password) {
         return todoService.delete(id, password);
     }

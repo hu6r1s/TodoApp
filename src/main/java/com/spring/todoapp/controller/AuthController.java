521c189626b5e7e811335e240b966713f0ea2c60
Hu6r1s <darmaa333@gmail.com>
2024-02-02 14:13:47 +0900

    feat: validation and valid exception

--- src/main/java/com/spring/todoapp/controller/AuthController.java
+++ src/main/java/com/spring/todoapp/controller/AuthController.java
@@ -1,12 +1,19 @@
 package com.spring.todoapp.controller;
 
+import com.spring.todoapp.common.exception.ExceptionResponse;
 import com.spring.todoapp.dto.RefreshTokenRequestDto;
 import com.spring.todoapp.dto.UserRequestDto;
 import com.spring.todoapp.service.AuthService;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +27,16 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/signup")
-    public ResponseEntity signup(@RequestBody UserRequestDto requestDto) {
+    public ResponseEntity signup(@RequestBody @Valid UserRequestDto requestDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            List<ExceptionResponse> exceptionResponseList = new ArrayList<>();
+            for (FieldError fieldError : fieldErrors) {
+                ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.BAD_REQUEST, fieldError.getDefaultMessage());
+                exceptionResponseList.add(exceptionResponse);
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionResponseList);
+        }
         return new ResponseEntity(authService.signup(requestDto), HttpStatus.CREATED);
     }
 
--- src/main/java/com/spring/todoapp/dto/UserRequestDto.java
+++ src/main/java/com/spring/todoapp/dto/UserRequestDto.java
@@ -1,9 +1,16 @@
 package com.spring.todoapp.dto;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UserRequestDto {
+    @Size(min = 4, max = 10, message = "Username은 4자 이상 10자 이하이어야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Username은 알파벳 소문자(a~z), 숫자(0~9)로 구성되어야 합니다.")
     private String username;
+
+    @Size(min = 8, max = 15, message = "Password는 8자 이상 15자 이하이어야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Password는 알파벳 대소문자(a~z, A~Z), 숫자(0~9)로 구성되어야 합니다.")
     private String password;
 }
